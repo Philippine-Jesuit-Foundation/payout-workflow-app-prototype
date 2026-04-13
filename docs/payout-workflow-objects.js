a92c03a3f5b88f5da065d6d72c05e8fc53e13293
pjfitacc <pjfitacc@gmail.com>
2026-04-13 17:13:00 -0400

    feat: AcknowledgmentLetterFormObject and EmailFormObject

--- docs/payout-workflow-objects.js
+++ docs/payout-workflow-objects.js
@@ -22,6 +22,7 @@
 // - Amount
 // - Beneficiary
 // - Donation Date
+// - Transaction ID
 
 
 // This object fulfills these requirements:
@@ -29,6 +30,7 @@
 // 2.The app allows users to provide additional data like comments to a paid out tx.
 // 3.The app allows users to copy paid out tx info + additional info into an excel sheet in our nonprofit Dropbox.
 const customerTransactionFormObject = {
+    transactionId: '', // from Stripe Transaction object, not editable
     email: '',
     phone: '',
     name: '',
@@ -52,14 +54,44 @@ const customerTransactionFormObject = {
 
 
 // Acknowledgment Letter Form Object
+// description:
+// This object represents the form for creating an acknowledgment letter for a paid out transaction. 
+// It includes fields for the custom message, paid out transaction info, and template selection for the acknowledgment letter. The app will use this form to generate a filled acknowledgment letter that can be attached to a receipt email or stored as a PDF in our nonprofit Dropbox.
+// This object is dependent on the Customer Transaction Form Object, as it will pull in the paid out transaction info from the Customer Transaction Form Object to automatically fill out the acknowledgment letter. The user cannot edit the paid out transaction info in the Acknowledgment Letter Form Object, but they can edit the custom message and select the template for the acknowledgment letter.
+
 // This object fulfills these requirements:
 // 1. The app allows users to use an input form that creates an acknowledgment letter with a custom message for a paid out tx. 
 // 2. The app will automatically fill out paid out tx info that the user optionally edited into the input form that creates the acknowledgement letter. 
 // 3. The app allows using different acknowledgment letter templates. 
+const acknowledgmentLetterFormObject = {
+    transactionId: '', // from Stripe Transaction object, not editable
+    customMessage: '',
+    // paid out transaction info pulled from the Customer Transaction Form Object (not editable in this form)
+    name: '',
+    email: '',
+    amount: 0,
+    beneficiary: '',
+    donationDate: null,
+    // template selection for the acknowledgment letter
+    template: 'default', // default template, but we can have other templates like 'business_donation', 'large_donation', etc.
+};
 
 
 // Email Form Object
+// description:
+// This object represents the form for sending a receipt email with a filled acknowledgment letter attached for a paid out transaction. 
+// Dependent on acknowledgment Letter Form Object, as it will pull in the filled acknowledgment letter generated from the Acknowledgment Letter Form Object to attach to the receipt email. The user can edit the subject, to, and message fields when sending the receipt email. When the user sends the receipt email, the app will also store the emailed acknowledgment letter as a PDF in our nonprofit Dropbox for record-keeping purposes.
+
 // This object fulfills these requirements:
 // 1. The app allows users to email a receipt with a filled acknowledgment letter attached for a paid out tx. 
 // 2. The app allows users to edit subject, to, and message when sending receipt email. 
 // 3. The subsystem should store emailed acknowledgment letters as pdfs in our nonprofit Dropbox. 
+const emailFormObject = {
+    transactionId: '', // from Stripe Transaction object, not editable
+    subject: '',
+    to: '',
+    message: '',
+    // filled acknowledgment letter generated from the Acknowledgment Letter Form Object (not editable in this form)
+    acknowledgmentLetter: null, // this will be a PDF file or a link to the PDF file stored in our nonprofit Dropbox
+};
+
